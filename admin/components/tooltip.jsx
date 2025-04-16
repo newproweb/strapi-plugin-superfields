f700c6ea756557fddab76216993047b82ebdf848
aliev <davlatbek.mamurjonov@newproweb.com>
2025-04-16 12:18:02 +0500

    text input component updated

--- admin/components/tooltip.jsx
+++ admin/components/tooltip.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { TextInput, Tooltip, Field } from "@strapi/design-system";
+import {
+  TextInput,
+  Tooltip,
+  Field,
+  DesignSystemProvider,
+  lightTheme ,
+  darkTheme
+} from "@strapi/design-system";
 import { Information } from "@strapi/icons";
-
+import {useSystemTheme} from '../hooks/use-system-theme'
 const TextInputComponent = ({
   onChange,
   name,
@@ -13,33 +20,37 @@ const TextInputComponent = ({
   const tooltipContent = attribute?.options["tooltip-content"] ?? "";
   const description = attribute?.options["description"] ?? "";
 
-  return (
-    <>
-      <Field.Root id={name} error={error} hint={description}>
-        <Field.Label style={{ marginBottom: "5px" }}>{name}</Field.Label>
+  const systemTheme = useSystemTheme();
+  const theme = systemTheme === 'dark' ? darkTheme : lightTheme;
 
-        <TextInput
-          onChange={(e) =>
-            onChange({ target: { name, value: e.target.value } })
-          }
-          value={value ?? ""}
-          id={name}
-          error={error}
-          name={name}
-          required={required}
-          endAction={
-            <Tooltip label={tooltipContent ?? ""}>
-              <Information
-                aria-hidden
-                style={{ zIndex: 99999, cursor: "pointer" }}
-              />
-            </Tooltip>
-          }
-        />
-        <Field.Error />
-        <Field.Hint />
-      </Field.Root>
-    </>
+
+  return (
+    <Field.Root id={name} error={error} hint={description}>
+      <Field.Label style={{ marginBottom: "5px" }}>{name}</Field.Label>
+
+      <TextInput
+        onChange={(e) => onChange({ target: { name, value: e.target.value } })}
+        value={value ?? ""}
+        id={name}
+        error={error}
+        name={name}
+        required={required}
+        endAction={
+          tooltipContent ? (
+            <DesignSystemProvider theme={theme}>
+              <Tooltip label={tooltipContent ?? ""}>
+                <Information
+                  aria-hidden
+                  style={{ cursor: "pointer" , color: systemTheme === 'dark' ? '#fff' : '#000'}}
+                />
+              </Tooltip>
+            </DesignSystemProvider>
+          ) : null
+        }
+      />
+      <Field.Error />
+      <Field.Hint />
+    </Field.Root>
   );
 };
 
